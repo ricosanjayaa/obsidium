a1d3d249662e8f166ddbc3715aabdfe8d42f4269
Rico Sanjaya <ricosanjayaa@gmail.com>
2023-07-31 19:48:16 +0700

    changes on navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,7 @@
-import ThemeToggler from "@/components/ThemeToggler";
-
 export default function Page() {
   return (
     <div>
       <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!</p>
-      <p>hello world!v</p>
     </div>
   )
 }
--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -6,9 +6,10 @@ export default function NavigationBar() {
     <div className="w-full text-[#222222] dark:text-[#E9E9E9] transition-all">
       <div className="p-6 mx-auto max-w-6xl flex flex-row justify-between items-center">
         <Link href="/">
-          <p className="text-xl font-medium">
-            obsidium/home
-          </p>
+          <div className="flex flex-row items-baseline">
+            <p className="text-2xl font-medium">@ricosnjy</p>
+            <p className="text-sm opacity-75">/obsidium</p>
+          </div>
         </Link>
         <ThemeToggler />
       </div>
