3e068445bc65a61945cc70f87b428e4924165da9
Rico Sanjaya <ricosanjayaa@gmail.com>
2023-08-05 11:12:41 +0700

    favicon changes

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -6,7 +6,7 @@ export default function NavigationBar() {
     <div className="w-full text-[#222222] dark:text-[#E9E9E9] transition-all">
       <div className="p-6 mx-auto max-w-6xl flex flex-row justify-between items-center">
         <Link href="/">
-          <img src="favicon.ico" className="w-8 brightness-75 hover:brightness-100 dark:brightness-50 dark:hover:brightness-100 invert dark:invert-0 transition-all duration-500" />
+          <img src="favicon.ico" className="w-8 invert opacity-50 hover:opacity-100 dark:opacity-50 dark:hover:opacity-100 dark:invert-0 transition-all duration-500" />
         </Link>
         <ThemeToggler />
       </div>
